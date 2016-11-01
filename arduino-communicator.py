59c2d6b3ac0f23410e872d62b1f024a1a8e54bd9
Pieter Staal <pieterstaal@outlook.com>
2016-11-01 12:17:22 +0100

    Add automatic port detection

--- arduino-communicator.py
+++ arduino-communicator.py
@@ -2,7 +2,7 @@ import serial
 import urllib3
 import json
 
-ser = serial.Serial('/dev/ttyACM0', 9600)
+#ser = serial.Serial('/dev/ttyACM0', 9600)
 http = urllib3.PoolManager()
 
 terminal_type = "Unknown"
@@ -16,6 +16,39 @@ else:
     print("Invalid answer")
     exit()
 
+port = input("What port are you on? If you don't know, just press enter.")
+
+ser = serial.Serial()
+
+if (port == ""):
+    for i in range(10):
+        try:
+            ser = serial.Serial('/dev/ttyACM' + str(i), 9600)
+            break
+        except serial.serialutil.SerialException:
+            continue
+else:
+    try:
+        ser = serial.Serial(port, 9600)
+    except serial.serialutil.SerialException:
+        pass
+
+if (ser.port == None and port == ""):
+    print("It looks like your card scanning device is not connected,"
+          " please plug it in this computer. \nIf it is plugged in, "
+          "try a different USB port or cable. \n\n\nERROR CODE 000")
+    exit()
+elif (ser.port == None and port != ""):
+    print("The program cannot find the card scanning device at the"
+          " given port. \nPlease try running the program again and"
+          " entering a different port. \nIf you are sure the port is"
+          " correct, try plugging in the card scanning device in a"
+          " different USB port or using a different USB cable. \n\n"
+          "\nERROR CODE 001")
+    exit()
+
+print("Succesfully established a connection with the card scanning device! \n"
+      "You can start scanning now.")
 
 def encrypt(text):
     return text
@@ -26,7 +59,7 @@ def serialize(data):
 def sendData(data):
     r = http.request(
         'POST',
-        '130.89.228.250:8080/Restaurant/PythonReceiver',
+        '130.89.231.249:8000/restaurant/cardswiped',
         headers={'Content-Type': 'application/json'},
         body=serialize(encrypt(data)))
     r.read()
@@ -52,4 +85,4 @@ while True:
 
         sendData(num_id)
     elif (data.startwith('KEY')):
-        sendData(data)        
+        sendData(data)
