09a260254e21cdc3ebf4665fa0ec7e792135c8ff
Myrthe <myrthesakura@gmail.com>
2016-11-02 15:05:39 +0100

    Fix bug possibly
    i hope this fixes a small bug

--- backend/restaurant/admin.py
+++ backend/restaurant/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from restaurant import models
+from django.db import models
 
 admin.site.register(models.Card)
 admin.site.register(models.Food)
@@ -8,4 +9,4 @@ admin.site.register(models.Account)
 admin.site.register(models.CardSwipe)
 
 class FoodAdmin(admin.ModelAdmin):
-    list_display = ('name')
+    list_display = (name)
